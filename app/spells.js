85675abcfb460d1ba0969ba1c6c9c94fd26d2ddc
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-07-18 22:16:06 -0400

    Adds buttons for saving and removing favorite spells. Right now, I just have the favorites appear at the bottom of the page until I figure out the UX for this.

--- app/spells.js
+++ app/spells.js
@@ -1,7 +1,45 @@
 var spellApp = angular.module('spell', []);
 
 spellApp.controller('SpellController', function($scope, $http){
-    $scope.spells= [];
+    $scope.spells = [];
+    $scope.saved = localStorage.getItem('spells');
+    $scope.savedSpells = (localStorage.getItem('spells') !== null)
+    											? JSON.parse($scope.saved)
+    											: [];
+
+    $scope.saveSpell = function(){
+			$scope.savedSpells.push($scope.selectedSpell);
+			localStorage.setItem('spells', JSON.stringify($scope.savedSpells));
+		};
+
+    $scope.removeSpell = function(spellName){
+    	var index = -1;
+    	for(var i = 0; i < $scope.savedSpells.length; i++){
+    		if($scope.savedSpells[i].name === spellName){
+					index = i;
+				}
+			}
+			
+			if(index > -1) {
+				$scope.savedSpells.splice(index, 1);
+				localStorage.setItem('spells', JSON.stringify($scope.savedSpells));
+			}
+		};
+
+		$scope.isSpellSaved = function(spellName){
+    	for(var i = 0; i < $scope.savedSpells.length; i++){
+    		if($scope.savedSpells[i].name === spellName){
+    			return true;
+				}
+			}
+
+			return false;
+		};
+
+		$scope.clearSavedSpells = function(){
+			localStorage.clear();
+			$scope.savedSpells = [];
+		};
 
     $http.get('data/spells.json')
         .success(function(results){
@@ -14,6 +52,7 @@ spellApp.controller('SpellController', function($scope, $http){
 						}
             $scope.spells = array;
 						$scope.selectedSpell = $scope.spells[0];
+						console.log($scope.savedSpells);
         })
 		.error(function(){
 			alert('ruh roh');	
